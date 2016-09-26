0d0392eb090a83a4d28c0f997fb5c505b7136cec
Alex Benoit <alexander.benoit@outlook.com>
2016-09-27 00:44:10 +0200

    changed transparancy on navbar

--- js/alexweb.navbar.js
+++ js/alexweb.navbar.js
@@ -1,4 +1,33 @@
 $(document).ready(function(){
+  var userHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+  var startFade = userHeight - userHeight/3;
+  var endFade = userHeight - 70;
+  var $navbar = $('#navbar');
+  var st = $(this).scrollTop();
+  if ( st <= startFade ) {
+    $navbar.css({ 'background' : 'rgba(11,28,72,0.0)' });
+  } else if ( st > startFade && st < endFade ) {
+    var transparancy = ((st/endFade - startFade/endFade) / (1 - startFade/endFade));
+    $navbar.css({ 'background' : `rgba(11,28,72,${transparancy})` });
+  } else {
+    $navbar.css({ 'background' : 'rgba(11,28,72,1)' });
+  }
+
+  $(window).on('scroll', function() {
+    var st = $(this).scrollTop();
+    if ( st <= startFade ) {
+      $navbar.css({ 'background' : 'rgba(11,28,72,0.0)' });
+    } else if ( st > startFade && st < endFade ) {
+      var transparancy = ((st/endFade - startFade/endFade) / (1 - startFade/endFade));
+      $navbar.css({ 'background' : `rgba(11,28,72,${transparancy})` });
+    } else {
+      $navbar.css({ 'background' : 'rgba(11,28,72,1)' });
+    }
+  });
+});
+
+
+$(document).ready(function(){
   $('#arrow-down-toAbout').on('click', function() {
     event.preventDefault();
     var targetOffset = $('#about').offset().top - 70;
@@ -34,21 +63,4 @@ $(document).ready(function(){
     var targetOffset = $('#contact').offset().top - 70;
     $("html, body").animate({scrollTop: targetOffset}, 500);
   });
-
-  var userHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-  var startFade = userHeight - userHeight/3;
-  var endFade = userHeight - 70;
-  var $navbar = $('#navbar');
-  $(window).on('scroll', function() {
-    var st = $(this).scrollTop();
-    if ( st <= startFade ) {
-      $navbar.css({ 'background' : 'rgba(11,28,72,0.2)' });
-    } else if ( st > startFade && st < endFade ) {
-      var transparancy = ((st/endFade - startFade/endFade)*0.8 / (1 - startFade/endFade) + 0.2);
-      $navbar.css({ 'background' : `rgba(11,28,72,${transparancy})` });
-    } else {
-      $navbar.css({ 'background' : 'rgba(11,28,72,1)' });
-    }
-  });
-
 });
